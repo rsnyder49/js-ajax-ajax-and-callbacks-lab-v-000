78b68759aaec1800782bd137dd14eb1715fa3c92
Robert Snyder <r.snyder49@gmail.com>
2018-12-28 22:28:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 $(document).ready(function (){
 });
+
+function displayError(error) {
+  $('#errors').html("There was an error, please try again.");
+ }
+ 
+function displayRepositories(result) {
+  return `<strong>Name: </strong>${result.name}<br>
+         <strong>Description: </strong>${result.description}<br>
+         <strong>URL: </strong><a href="${result.html_url}">${result.html_url}</a><br>
+         <a href="#" data-repository="${result.name}" data-owner="${result.owner.login}"
+           onclick="showCommits(this)">Show Commits</a><br></br>`;
+}
